604fa78a12b8035bc00078e4a6c3458e423ed772
zetahawke <michel.szinavel@gmail.com>
2015-10-23 04:39:07 -0300

    db changed from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151021013015) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "account_types", force: :cascade do |t|
     t.string   "name"
     t.integer  "priviledges"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20151021013015) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -60,7 +63,8 @@ ActiveRecord::Schema.define(version: 20151021013015) do
     t.integer  "account_type"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "identities", "users"
 end
