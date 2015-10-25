1da3100eeff9227250eaa56bbda8234569e1b4de
hirochi <hirosmans@gmail.com>
2015-10-25 01:05:47 -0300

    feat(): specify layout to application and add rbenv-var example file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
+  protect_from_forgery with: :null_session
+  layout 'application'
+
   def ensure_signup_complete
     # Ensure we don't go into an infinite loop
     return if action_name == 'finish_signup'
