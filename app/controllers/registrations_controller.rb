b43e92984114065ac87c30eb67c77c99d7b55857
zetahawke <michel.szinavel@gmail.com>
2015-12-18 10:45:41 -0300

    fixed(): account type in the passenger create account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,12 +7,13 @@ class RegistrationsController < Devise::RegistrationsController
 	def create
 		
 		@user = Passenger.new(email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+		#binding.pry
+    	@user.account_type = 2
     	if @user.save
     		if @user.active_for_authentication?
     			#respond_with @user, location: after_sign_up_path_for(@user)
     			#sign_in(@user, :bypass => true)
     			#@user.skip_reconfirmation!
-    			#binding.pry
         		sign_in(:user, @user.acting_as)
         		redirect_to "/profile/#{@user.acting_as.id}", notice: 'Your profile was successfully updated.'
     		end
